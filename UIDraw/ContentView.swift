d7a49519d3ad3ecea0d7c1f4c40f1bf3cb4e289f
Jordan Singer <singer.jordan@gmail.com>
2023-11-24 14:56:21 -0500

    Initial Commit

---
+++ UIDraw/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIDraw
+//
+//  Created by Jordan Singer on 11/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIDraw/UIDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIDrawApp.swift
+//  UIDraw
+//
+//  Created by Jordan Singer on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct UIDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
